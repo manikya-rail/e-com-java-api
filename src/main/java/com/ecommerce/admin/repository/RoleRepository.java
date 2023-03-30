77ab4be1764af6becf52a434f540f99d657b1d4d
Nayana Jayaraj <njayaraj50@gmail.com>
2023-03-30 17:57:55 +0530

    configuration issue resolved

--- src/main/java/com/ecommerce/admin/repository/RoleRepository.java
+++ src/main/java/com/ecommerce/admin/repository/RoleRepository.java
@@ -1,5 +1,11 @@
 package com.ecommerce.admin.repository;
 
-public interface RoleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.admin.entity.Roles;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Roles, Integer>{
 
 }
