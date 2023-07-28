33853c6ab5fde4e8f7a23d415af866f0a00d6154
jenobjob <jenobvarghese@techversantinfotech.com>
2023-07-28 14:44:29 +0530

    create service registry for registering all services

---
+++ service-registry/src/test/java/com/techversantinfotech/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.techversantinfotech.serviceregistry;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ServiceRegistryApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
