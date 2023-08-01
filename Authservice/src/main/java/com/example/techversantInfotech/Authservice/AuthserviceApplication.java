b35fe33be44f6e577e3886c9c6d6d13a27d1566f
jenobjob <jenobvarghese@techversantinfotech.com>
2023-08-01 16:52:58 +0530

    enhance the code

--- Authservice/src/main/java/com/example/techversantInfotech/Authservice/AuthserviceApplication.java
+++ Authservice/src/main/java/com/example/techversantInfotech/Authservice/AuthserviceApplication.java
@@ -3,6 +3,7 @@ package com.example.techversantInfotech.Authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
