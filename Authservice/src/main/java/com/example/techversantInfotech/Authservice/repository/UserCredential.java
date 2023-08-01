6a9329c8a68ff6b8b9ad4e8287932c7af6a49bee
jenobjob <jenobvarghese@techversantinfotech.com>
2023-08-01 16:54:42 +0530

    add finduserbyemailmethod

--- Authservice/src/main/java/com/example/techversantInfotech/Authservice/repository/UserCredential.java
+++ Authservice/src/main/java/com/example/techversantInfotech/Authservice/repository/UserCredential.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserCredential extends JpaRepository<User,Integer> {
     Optional<User> findByUsername(String username);
+    Optional<User>findByEmail(String  email);
 }
