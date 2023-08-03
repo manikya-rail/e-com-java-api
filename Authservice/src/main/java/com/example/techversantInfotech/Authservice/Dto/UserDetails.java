38ddb39c1bf3dc05771a60650eae10094e62c756
jenobjob <jenobvarghese@techversantinfotech.com>
2023-08-03 19:18:18 +0530

    enhance userDetails

--- Authservice/src/main/java/com/example/techversantInfotech/Authservice/Dto/UserDetails.java
+++ Authservice/src/main/java/com/example/techversantInfotech/Authservice/Dto/UserDetails.java
@@ -5,12 +5,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDetails {
     private int id;
+    private String name;
+    private String mobileNumber;
+    private byte[] image;
+    private String description;
+    private boolean active;
+    private boolean delete;
+    private Date modifiedOn;
+    private Date createdOn;
     private String username;
     private String email;
     private String role;
