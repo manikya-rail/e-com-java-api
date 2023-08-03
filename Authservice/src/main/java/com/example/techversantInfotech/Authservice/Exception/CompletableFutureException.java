4f7f264fc22e33172ea9e3789c94b06dbfa1856c
jenobjob <jenobvarghese@techversantinfotech.com>
2023-08-03 19:17:46 +0530

    add new custome exception

--- Authservice/src/main/java/com/example/techversantInfotech/Authservice/Exception/CompletableFutureException.java
+++ Authservice/src/main/java/com/example/techversantInfotech/Authservice/Exception/CompletableFutureException.java
@@ -1,2 +1,19 @@
-package com.example.techversantInfotech.Authservice.Exception;public class CompletableFutureException {
+package com.example.techversantInfotech.Authservice.Exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CompletableFutureException extends RuntimeException{
+    private String  errorCode;
+    public CompletableFutureException(String message, String errorCode){
+        super(message);
+        this.errorCode=errorCode;
+    }
+
 }
--- Authservice/src/main/java/com/example/techversantInfotech/Authservice/Exception/ImageProcessingException.java
+++ Authservice/src/main/java/com/example/techversantInfotech/Authservice/Exception/ImageProcessingException.java
@@ -2,12 +2,14 @@ package com.example.techversantInfotech.Authservice.Exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ImageProcessingException extends Exception{
+public class ImageProcessingException extends RuntimeException{
     private String  errorCode;
     public ImageProcessingException(String message, String errorCode){
         super(message);
