159dc71edfdd047fed5668957aa9928d68b81eb8
Nayana Jayaraj <njayaraj50@gmail.com>
2023-09-01 16:04:27 +0530

    integration testing

--- Authservice/src/test/java/com/ecommerce/techversantInfotech/Authservice/controller/AuthControllerTest.java
+++ Authservice/src/test/java/com/ecommerce/techversantInfotech/Authservice/controller/AuthControllerTest.java
@@ -5,9 +5,11 @@ import com.ecommerce.techversantInfotech.Authservice.Entity.Test_User;
 import com.ecommerce.techversantInfotech.Authservice.Exception.UserAlreadyRegistered;
 import com.ecommerce.techversantInfotech.Authservice.enumDetails.UserRole;
 import com.ecommerce.techversantInfotech.Authservice.repository.TestRepo;
+import com.ecommerce.techversantInfotech.Authservice.service.AuthService;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
@@ -15,9 +17,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
@@ -50,6 +54,9 @@ class AuthControllerTest{
 
     @Autowired
     private TestRepo testRepo;
+    
+    @MockBean
+    private AuthService authService;
 
     @BeforeEach
     public void setUp(){
@@ -162,5 +169,34 @@ class AuthControllerTest{
 
     }
 
+    @Test
+    public void testGetUserById() throws Exception {
+        // Prepare a test user
+        Test_User testUser = Test_User.builder()
+                .id(1)
+                .name("jenob")
+                .username("jeb")
+                .email("jenob@gmail.com")
+                .password("12345")
+                .location("kochi")
+                .Description("Hi this is test purpose")
+                .mobileNumber("9840280239")
+                .role(UserRole.SUPER_ADMIN)
+                .build();
 
+        // Mock the AuthService to return the test user when getById is called
+        Optional<Test_User> user=testRepo.findById(1);
+
+        // Perform the GET request
+        mockMvc.perform(MockMvcRequestBuilders.get("/client/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("jenob"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username").value("jeb"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("jenob@gmail.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.location").value("kochi"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.description").value("Hi this is test purpose"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.mobileNumber").value("9840280239"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.role").value("SUPER_ADMIN"));
+    }
 }
